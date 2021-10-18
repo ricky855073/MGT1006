109131388c60a1448a51be9650fbc26ee73a21d6
RickyLiu <ricky855073@gmail.com>
2021-10-18 19:47:56 +0800

    作業2 仍須修改 14/30

--- Week3/hw2_3.py
+++ Week3/hw2_3.py
@@ -1,43 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 17 18:16:23 2021
-
-@author: hsienmingliu
-"""
 n = int(input())
 
 price_star = 0
 max_profit = 0
 best_i = 0
-no_profit = 0
-
 
 for i in range(1, n + 1):
-    # print(f"This i is {i}")
     a = int(input())
     b = int(input())
     c = int(input())
 
     if c > a:
-        no_profit += 1
         price = 1000
         profit = 0  # profit
-        if no_profit == 1:
-            if max_profit == 0 and price_star == 0:
-                best_i = i
-                max_profit = profit
-                price_star = price
-
+        if max_profit != 0 or price_star != 1000:
+            best_i = i
+            max_profit = 0
+            price_star = 1000
     else:
-        for price in range(a // b, c - 1, -1):
+        for price in range(c + 1, a // b):
             profit = (a - b * price) * (price - c)
-            if profit > max_profit:
-                best_i = i
-                max_profit = profit
-                price_star = price
-            elif profit == max_profit:
-                max_profit = profit
-                price_star = price
+            if profit >= max_profit and price > price_star:
+                if price > price_star:
+                    best_i = i
+                    max_profit = profit
+                    price_star = price
+            else:
+                break
 
 print(best_i, int(price_star), int(max_profit), sep=",")
