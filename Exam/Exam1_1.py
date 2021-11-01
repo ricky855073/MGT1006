1579a2733feab588c82b155941fb30e362c12cd7
RickyLiu <ricky855073@gmail.com>
2021-11-01 09:43:36 +0800

    Sample Testing Data AC

---
+++ Exam/Exam1_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 09:18:44 2021
+
+@author: hsienmingliu
+"""
+
+input_1 = int(input())
+input_2 = int(input())
+input_3 = int(input())
+input_4 = int(input())
+summation = 0
+
+summation = (1000 * input_1 +
+            100 * input_2 +
+            10 * input_3 +
+            1 * input_4)
+
+print(summation)
