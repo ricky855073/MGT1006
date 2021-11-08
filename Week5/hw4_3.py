3e0f728f4ea37f028ae6278a7947b432fcc15c43
RickyLiu <ricky855073@gmail.com>
2021-11-08 22:47:47 +0800

    已AC

--- Week5/hw4_3.py
+++ Week5/hw4_3.py
@@ -7,9 +7,9 @@ input_2 = input()
 n = int(input_1.split(",")[0])
 fire_dept = int(input_1.split(",")[1])  # k
 catch_fire = list()  # h
-city_matrix = list() # 2d array constructed by list
+city_matrix = list()  # 2d array constructed by list
 fire_dept_building = list()
-total_path = int()
+total_path = 0
 
 for i in range(n):
     catch_fire.append(int(input_2.split(",")[i]))
@@ -36,15 +36,22 @@ fire_dept_building.append(min(first_loc)[1])
 for i in range(fire_dept - 1):
     max_distance = int()
     each_distance = list()
+
     for j in range(n):
+        each_path_min = 1001  # for default purpose
+        fire_dept_candidate = 1  # for default purpose
         if j + 1 not in fire_dept_building:
             for k in range(n):
-                if k + 1 in fire_dept_building:
-                    each_distance.append([city_matrix[j][k], j + 1])
+                if ((k + 1 in fire_dept_building)
+                        and (city_matrix[j][k] < each_path_min)):
+                    each_path_min = city_matrix[j][k]
+                    fire_dept_candidate = j + 1
+            each_distance.append([each_path_min, fire_dept_candidate])
+
     temp_list = list()
-    for l in range(len(each_distance)):
-        if (max(each_distance)[0]) == (each_distance[l][0]):
-            temp_list.append(each_distance[l])
+    for m in range(len(each_distance)):
+        if (max(each_distance)[0]) == (each_distance[m][0]):
+            temp_list.append(each_distance[m])
     fire_dept_building.append(min(temp_list)[1])
 
 # Calculate total path
@@ -61,7 +68,4 @@ for i in range(len(fire_dept_building)):
     if i != len(fire_dept_building) - 1:
         print(",", end="")
 print(";", end="")
-print(total_path, end = "")
-
-
-
+print(total_path, end="")
