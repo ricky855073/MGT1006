812c7ecb59b514cd5e62bd97395b37ee7e9a73df
RickyLiu <ricky855073@gmail.com>
2021-11-13 21:15:31 +0800

    已AC

--- Week6/hw5_2.py
+++ Week6/hw5_2.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 11 16:14:12 2021
+Created on Sat Nov 13 21:05:33 2021
 
 @author: hsienmingliu
 """
 
-
-def mono_inc(inlist, k=3):
+def mono_inc_plus(inlist, k=3):
     # len_inlist = n, len_diff_list = n - 1
     if k < 3:
         k = 3
@@ -29,7 +28,8 @@ def mono_inc(inlist, k=3):
         continue_list.append([counter, len(diff_list)])
 
     if continue_list != list():
-        print(f'{continue_list[0][1] - continue_list[0][0]}\n{continue_list[0][1]}')
+        for i in range(len(continue_list)):
+            print(f'{continue_list[i][1] - continue_list[i][0]} {continue_list[i][1]}')
     else:
         print(None)
 
@@ -39,8 +39,9 @@ def main():
     test_positive = input_1.split(",")
     test_positive = list(map(int, test_positive))
     k = int(input())  # k value
-    mono_inc(test_positive, k)
+    mono_inc_plus(test_positive, k)
 
 
 if __name__ == '__main__':
     main()
+
