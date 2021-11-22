0c24f374e9bd4f56ce2b49323e3d0aba861c7640
RickyLiu <ricky855073@gmail.com>
2021-11-22 10:56:06 +0800

    WA 剩兩個測資錯(2, 8)

--- Week7/hw6_3.py
+++ Week7/hw6_3.py
@@ -10,24 +10,29 @@ string = input()
 # string = "Well-known examples include kernel-based approaches, such as Gaussian process regression model (GPR) [1] and support vector regression (SVR) [2], ensemble-based approaches, such as random forest [3], gradient boosting machines [4], and neural-network-based methods [5]."
 string_list = string.split()
 target_acronym_list = list()
+punctuation = '。，、；：「」『』（）─？！─…﹏《》〈〉．～　,.; !"#$%&\'()*+,-./:;<=>?@[]^_`{¦}~'
 
 counter = 0
 
 for word in string_list:
     matching_error = 0
     if "(" in word:
-        processed_word = word.replace("(", "").replace(")", "")
-        length_word = len(processed_word)
-        target_word_list = string_list[counter - length_word:counter]
-        word_counter = 0
-        for j in target_word_list:
-            if processed_word[word_counter].lower() != j[0].lower():
-                matching_error += 1
-                break
-            word_counter += 1
-        if matching_error == 0:
-            fullname = " ".join(target_word_list)
-            target_acronym_list.append([processed_word, fullname])
+        if word.isupper():
+            processed_word = word.replace("(", "").replace(")", "")
+            for punct in punctuation:
+                if punct in processed_word:
+                    processed_word = processed_word.replace(punct, "")
+            length_word = len(processed_word)
+            target_word_list = string_list[counter - length_word:counter]
+            word_counter = 0
+            for j in target_word_list:
+                if processed_word[word_counter].lower() != j[0].lower():
+                    matching_error += 1
+                    break
+                word_counter += 1
+            if matching_error == 0:
+                fullname = " ".join(target_word_list)
+                target_acronym_list.append([processed_word, fullname])
     counter += 1
 
 for i in range(len(target_acronym_list)):
