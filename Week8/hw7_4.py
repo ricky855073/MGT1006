8ff7abf6acc106cbbdaddc98d3b8064c5569777f
RickyLiu <ricky855073@gmail.com>
2021-12-04 17:37:09 +0800

    刪除新增全部的pair_list, 改為有遇到再新增, 修改industry

--- Week8/hw7_4.py
+++ Week8/hw7_4.py
@@ -7,14 +7,15 @@ Created on Fri Dec  3 19:53:14 2021
 """
 
 # filepath = input()
-filepath = "/Users/hsienmingliu/Desktop/NTU/MGT1006/Week8/bgm_h1k.csv"
+filepath = "/Users/hsienmingliu/Desktop/NTU/MGT1006/Week8/test4.csv"
 
 
 process_list = list()
 string_list = list()
 industry_dict = dict()
 dict_keys_list = list()
-result_list = list()
+result_dict = dict()
+
 
 def data_preprocess():
     with open(filepath, "r", encoding = "utf-8") as f:
@@ -42,70 +43,73 @@ def data_preprocess():
     return process_list
 
 
-def median(lst):
-    quotient, remainder = divmod(len(lst), 2)
-    if remainder:
-        return sorted(lst)[quotient]
-    return int(round(sum(sorted(lst)[quotient - 1:quotient + 1]) / 2.))
+def counting(first, second, num_list):
+    global result_dict, compare_list
+    compare_list = f'{num_list[first]} {num_list[second]}'
+
+    if compare_list in result_dict:
+        count = result_dict[compare_list] + 1
+        result_dict.update({compare_list:count})
+    else:
+        result_dict.update({compare_list:1})
 
 
 def industry():
-    global dict_keys_list, result_list, pair_list, pair_result_list
+    global dict_keys_list, result_dict
     for row in process_list[1:]:
         industry_dict[row[8]] = row[9]
         industry_dict[row[10]] = row[11]
         industry_dict[row[12]] = row[13]
         industry_dict[row[14]] = row[15]
-    industry_dict.pop('')
+    try:
+        industry_dict.pop('')
+    except KeyError:
+        pass
 
     dict_keys_list = list(industry_dict.keys())
     dict_keys_list.sort()
 
-    pair_list = list()
-    counter = 0
-    for i in range(len(dict_keys_list)):
-        for j in range(len(dict_keys_list)):
-            if j > i:
-                pair_list.append([dict_keys_list[i], dict_keys_list[j]])
-                counter += 1
+    for row in process_list[1:]:
+        num_list = list()
+        for i in [row[8], row[10], row[12], row[14]]:
+            if i != '':
+                num_list.append(i)
             else:
-                continue
-
-    pair_result_list = list()
-    for pair in pair_list:
-        pair_counter = 0
-        for row in process_list[1:]:
-            if (row[8] in pair) and (row[10] in pair):
-                pair_counter += 1
-        if pair_counter > 0:
-            pair_result_list.append([pair, pair_counter])
-
-
-    # for item in dict_keys_list:
-    #     counter = 0
-    #     money_list = list()
-    #     for row in process_list[1:]:
-    #         if item == row[8] and industry_dict[item] == row[9]:
-    #             counter += 1
-    #             money_list.append(int(row[4]))
-    #         if item == row[10] and industry_dict[item] == row[11]:
-    #             counter += 1
-    #             money_list.append(int(row[4]))
-    #         if item == row[12] and industry_dict[item] == row[13]:
-    #             counter += 1
-    #             money_list.append(int(row[4]))
-    #         if item == row[14] and industry_dict[item] == row[15]:
-    #             counter += 1
-    #             money_list.append(int(row[4]))
-    #     result_list.append([item, median(money_list), counter])
-    # result_list = sorted(result_list, key = lambda i : (-i[1], i[0]))
+                break
+        num_list.sort()
+        if len(num_list) < 2:
+            continue
+        elif len(num_list) == 2:
+            counting(0, 1, num_list)
+        elif len(num_list) == 3:
+            counting(0, 1, num_list)
+            counting(0, 2, num_list)
+            counting(1, 2, num_list)
+        elif len(num_list) == 4:
+            counting(0, 1, num_list)
+            counting(0, 2, num_list)
+            counting(0, 3, num_list)
+            counting(1, 2, num_list)
+            counting(1, 3, num_list)
+            counting(2, 3, num_list)
+    result_dict = dict(sorted(result_dict.items(), key=lambda i: (-i[1], i[0])))
 
 
+def result_output(result_dict):
+    global limited_dict
+    limited_dict = dict(list(result_dict.items())[0:20])
+    for k, v in limited_dict.items():
+        index_list = k.split(" ")
+        for index in index_list:
+            print(industry_dict[index], end=" ")
+        print(v, end="")
+        print()
 
 
 def main():
     data_preprocess()
     industry()
+    result_output(result_dict)
 
 
 if __name__ == "__main__":
